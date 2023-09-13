29bf50cbcba6eb6761430b898aa7e68dd247b282
anji-mito <120057673+anji-mito@users.noreply.github.com>
2023-09-13 08:17:03 -0700

    fix: endpoint

--- stats/dto/src/main/java/ru/practicum/ewm/EndpointHitDto.java
+++ stats/dto/src/main/java/ru/practicum/ewm/EndpointHitDto.java
@@ -1,9 +1,15 @@
 package ru.practicum.ewm;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.time.LocalDateTime;
 
+@Getter
+@Setter
+@Builder
 public class EndpointHitDto {
     private String app;
     private String uri;
--- stats/dto/src/main/java/ru/practicum/ewm/ViewStatsDto.java
+++ stats/dto/src/main/java/ru/practicum/ewm/ViewStatsDto.java
@@ -1,5 +1,12 @@
 package ru.practicum.ewm;
 
+import lombok.*;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Builder
+@Setter
 public class ViewStatsDto {
     private String app;
     private String uri;
