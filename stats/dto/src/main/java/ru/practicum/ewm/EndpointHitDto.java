989f9be8721b8f780e2d12d8c747ca3b4090f8ab
anji-mito <120057673+anji-mito@users.noreply.github.com>
2023-09-13 07:31:06 -0700

    fix:

--- stats/dto/src/main/java/ru/practicum/ewm/EndpointHitDto.java
+++ stats/dto/src/main/java/ru/practicum/ewm/EndpointHitDto.java
@@ -1,10 +1,13 @@
 package ru.practicum.ewm;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 import java.time.LocalDateTime;
 
 public class EndpointHitDto {
     private String app;
     private String uri;
     private String ip;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
     private LocalDateTime timestamp;
 }
