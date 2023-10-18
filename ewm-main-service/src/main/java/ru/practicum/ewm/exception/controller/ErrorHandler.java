74fae341119370a0ded415abfaf4adb543c3c441
anji-mito <120057673+anji-mito@users.noreply.github.com>
2023-10-18 17:48:50 +0300

    style:

--- ewm-main-service/src/main/java/ru/practicum/ewm/exception/controller/ErrorHandler.java
+++ ewm-main-service/src/main/java/ru/practicum/ewm/exception/controller/ErrorHandler.java
@@ -2,7 +2,6 @@ package ru.practicum.ewm.exception.controller;
 
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
