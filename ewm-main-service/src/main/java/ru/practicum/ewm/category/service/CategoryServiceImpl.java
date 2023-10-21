0714f254361af85bf44f87ac873b46e293620771
anji-mito <120057673+anji-mito@users.noreply.github.com>
2023-10-21 23:30:59 +0300

    3 fix

--- ewm-main-service/src/main/java/ru/practicum/ewm/category/service/CategoryServiceImpl.java
+++ ewm-main-service/src/main/java/ru/practicum/ewm/category/service/CategoryServiceImpl.java
@@ -1,10 +1,12 @@
 package ru.practicum.ewm.category.service;
 
+import lombok.AllArgsConstructor;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import ru.practicum.ewm.category.dto.CategoryDto;
 import ru.practicum.ewm.category.dto.NewCategoryDto;
 import ru.practicum.ewm.category.mapper.CategoryMapper;
+import ru.practicum.ewm.category.model.Category;
 import ru.practicum.ewm.category.repository.CategoryRepository;
 import ru.practicum.ewm.exception.NotFoundException;
 
@@ -13,15 +15,11 @@ import java.util.List;
 import java.util.Objects;
 import java.util.stream.Collectors;
 
+@AllArgsConstructor
 @Service
 public class CategoryServiceImpl implements CategoryService {
     private final CategoryMapper categoryMapper;
     private final CategoryRepository categoryRepository;
-
-    public CategoryServiceImpl(CategoryMapper categoryMapper, CategoryRepository categoryRepository) {
-        this.categoryMapper = categoryMapper;
-        this.categoryRepository = categoryRepository;
-    }
 
     @Override
     public CategoryDto create(NewCategoryDto dto) {
@@ -39,11 +37,11 @@ public class CategoryServiceImpl implements CategoryService {
     @Override
     @Transactional
     public CategoryDto update(Long id, CategoryDto dto) {
-        var categoryToUpdateOptional = categoryRepository.findById(id);
-        var categoryToUpdate = categoryToUpdateOptional
+        Category categoryToUpdate = categoryRepository
+                .findById(id)
                 .orElseThrow(() -> new NotFoundException("Категория с id " + id + " не была найдена"));
         var name = dto.getName();
-        if (name != null && name.length() > 0 && !Objects.equals(name, categoryToUpdate.getName())) {
+        if (!Objects.equals(name, categoryToUpdate.getName())) {
             categoryToUpdate.setName(name);
         }
         return categoryMapper.toDto(categoryToUpdate);
